fa4e4fe858725680ed204bda5a500edb386e572d
Arielson13 <arielsonsousa37@gmail.com>
2025-11-09 15:28:44 -0300

    A estrutura do backend foi refinada. Todas foram adicionadas. Arquivo de config da vercel foi adicionado pra funcionalidade do deploy.

---
+++ api/routes/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { perguntar } = require("../config/gnai.js");
+const router = express.Router();
+
+router.get("/initial", async (req, res) => {
+    return "Hello World";
+})
+
+router.post("/perguntar", async (req, res) => {
+    const { prompt } = req.body;
+    if (typeof prompt !== "string" || prompt.trim() === "") {
+        return res.status(400).json({ error: "Prompt inválido" });
+    }
+
+    try {
+        const resposta = await perguntar(prompt);
+        res.json({ resposta });
+    } catch (err) {
+        console.error("Erro ao processar prompt:", err);
+        res.status(500).json({ error: "Erro interno ao processar o prompt" });
+    }
+});
+
+module.exports = router;
---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const routes = require("./routes/routes.js")
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
